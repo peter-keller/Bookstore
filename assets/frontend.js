4de3c3238f404c11700e522abe7f577c808bb397
peter-keller <keller.f.peter@gmail.com>
2018-01-24 15:44:11 +0100

    feat(frontend): booklist function created

--- assets/frontend.js
+++ assets/frontend.js
@@ -14,3 +14,16 @@ function ajax(method, resource, callback){
     };
     xhr.send();
 };
+
+
+function listDetails(response) {
+    response.books.forEach(function(element) {
+        const listData = '<tr><td>' + element.book_name +
+                         '</td><td>' + element.aut_name +
+                         '</td><td>' + element.cate_descrip +
+                         '</td><td>' + element.pub_name +
+                         '</td><td>' + element.book_price +
+                         '</td></tr>';
+        tableElement.innerHTML += listData;
+    });
+};
